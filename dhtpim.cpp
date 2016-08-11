27586f22c99f391555d46f40b40603377071ac6c
Matthieu Daumas <daumasm@bde.enseeiht.fr>
2016-08-11 11:54:04 +0200

    Move keychain_tracker to keychain_tracker.hpp

--- dhtpim.cpp
+++ dhtpim.cpp
@@ -3,88 +3,11 @@
 #include <iostream>
 #include <unordered_map>
 
-#include <opendht.h>
-
+#include "keychain_tracker.hpp"
 #include "cli_tools.hpp"
 #include "keychain.hpp"
 #include "tools.hpp"
 #include "msg.hpp"
-
-template<size_t pool_size, size_t period> struct keychain_tracker;
-
-// Follow and listen a moving keychain.
-template<size_t pool_size, size_t period = 60000>
-struct keychain_tracker
-{
-    keychain_tracker(
-        dht::DhtRunner& node,
-        const keychain<period>& chain,
-        map_type& map)
-    : node{node}, chain{chain}, map{map}
-    {
-        // Reset listener pool.
-        for(size_t i = 0; i < pool_size; ++i)
-            tokens[i].first = 0;
-
-        // Start listenning the current period.
-        update(false);
-
-        // Start a thread which will track each period.
-        holding = true;
-        this->holder = std::make_unique<std::thread>(
-            [this]()
-            {
-                disp("Starting keychain tracking thread...");
-                while(this->hold())
-                {
-                    this->update();
-                    std::this_thread::sleep_for(
-                        std::chrono::milliseconds(period));
-                }
-
-                return;
-            });
-    }
-
-    bool hold() const
-    {
-        return holding;
-    }
-
-    ~keychain_tracker()
-    {
-        if(this->holding)
-        {
-            this->holding = false;
-            this->holder->join();
-        }
-    }
-
-private:
-    void update(size_t ahead_of = period)
-    {
-        auto target = chain.ahead(ahead_of);
-        tokens[cursor] = std::make_pair(listen(node, target, map), target);
-
-        auto next_cursor = (cursor + 1) % pool_size;
-        if(tokens[next_cursor].first)
-            node.cancelListen(
-                dht::InfoHash(tokens[next_cursor].second),
-                tokens[next_cursor].first);
-
-        next_cursor = cursor;
-    }
-
-    bool holding = false;
-    std::unique_ptr<std::thread> holder;
-
-    size_t cursor = 0;
-    std::array<std::pair<size_t, std::string>, pool_size> tokens;
-
-    dht::DhtRunner& node;
-    const keychain<period>& chain;
-    map_type& map;
-};
 
 int main(int argc, char** argv)
 {
